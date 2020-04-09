aaf9d5ae7f956cc7b9ee95599fbc715ce27611b2
LighghtEeloo <LighghtEeloo@eeloo.kerbal>
2020-04-09 18:54:45 +0800

    Writein

--- springGui.py
+++ springGui.py
@@ -5,31 +5,47 @@
 
 import sys
 from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QFont, QColor, QCursor, QKeyEvent
 from PyQt5.QtWidgets import (
-    QWidget, QLCDNumber, QSlider, QVBoxLayout, QApplication)
+    QLCDNumber, QSlider, QVBoxLayout, 
+    QWidget, QApplication, QFrame
+)
 
-def pt(asd):
-    print(asd)
 
-class SigSlot(QWidget):
-    def __init__(self, parent=None):
-        QWidget.__init__(self)
+class Mainwindow(QWidget):
+    def __init__(self, parent=None, flags=Qt.WindowFlags()):
+        super().__init__(parent=parent, flags=flags)
         self.setWindowTitle('XXOO')
-        lcd = QLCDNumber(self)
-        slider = QSlider(Qt.Horizontal, self)
+        self.resize(500,500)
 
-        vbox = QVBoxLayout()
-        vbox.addWidget(lcd)
-        vbox.addWidget(slider)
-
-        self.setLayout(vbox)
-
-        slider.valueChanged.connect(lcd.display)
-        slider.valueChanged.connect(pt) # 其实就是函数回调
-        self.resize(350, 250)
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    mainwindow = Mainwindow()
+    mainwindow.show()
+    sys.exit(app.exec_())
 
 
-app = QApplication(sys.argv)
-qb = SigSlot()
-qb.show()
-sys.exit(app.exec_())
+# def pt(asd):
+#     print(asd)
+
+# class SigSlot(QWidget):
+#     def __init__(self, parent=None):
+#         QWidget.__init__(self)
+#         self.setWindowTitle('XXOO')
+#         lcd = QLCDNumber(self)
+#         slider = QSlider(Qt.Horizontal, self)
+
+#         vbox = QVBoxLayout()
+#         vbox.addWidget(lcd)
+#         vbox.addWidget(slider)
+
+#         self.setLayout(vbox)
+
+#         slider.valueChanged.connect(lcd.display)
+#         slider.valueChanged.connect(pt) # 其实就是函数回调
+#         self.resize(350, 250)
+
+# app = QApplication(sys.argv)
+# qb = SigSlot()
+# qb.show()
+# sys.exit(app.exec_())
